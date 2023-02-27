f5c1646c67b3210c710192a3fda079369cee33cb
DCoppee <dominiquecoppee.pro@gmail.com>
2023-02-27 16:49:54 +0100

    correction responsive

--- vite-project/src/components/Check.jsx
+++ vite-project/src/components/Check.jsx
@@ -3,17 +3,14 @@ import '/src/styles/Check.scss';
 
 const Check = () => {
     return (
-        <div className='div-check'>
-            <div className='div-item'>
-                <input type="checkbox" id="item1"/>
-                <label htmlFor="item1">Learn React</label>
-            </div>
-            <div className='div-item'>
-                <input type="checkbox" id="item2"/>
-                <label htmlFor="item2">Be awesome</label>
-            </div>
-        </div>
-
+        <ul className='div-check'>
+            <li className='div-item'>
+                 <input type="checkbox" /> First task
+            </li>
+            <li>
+                <input type="checkbox" /> Second task
+            </li>
+        </ul>
     );
 };
 
